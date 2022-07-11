35fc44ba9dff7b37a49d06a51760e3cef8fe1d25
Afua <afua.danquah@sky.uk>
2022-07-11 09:47:23 +0100

    changed the Component Scan path in ComponentConfig.java file

--- ca-functional/src/test/java/uk/sky/ovp/ca/functional/config/ComponentConfig.java
+++ ca-functional/src/test/java/uk/sky/ovp/ca/functional/config/ComponentConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("uk.sky.ovp.cp.functional.*")
+@ComponentScan("uk.sky.ovp.ca.functional.*")
 public class ComponentConfig {
 }
